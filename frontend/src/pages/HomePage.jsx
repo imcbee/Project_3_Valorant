eaf1fa1be17ef737be16c217eaacfe182a33af9a
Ian <imcbee@terpmail.umd.edu>
2022-09-19 19:58:38 -0400

    updates

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,10 @@ import LeaderBoard from "../components/LeaderBoard";
 import React from "react";
 import "../styles/HomePage.css";
 
+//
+
 export default function HomePage() {
+  //pass User Auth useState into components to render data
   return (
     <>
       <div class="v3_4">
