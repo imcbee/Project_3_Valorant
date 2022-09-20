ad5d2fe8c98dea657c23e795f712c7c15b03ace1
Kingsley <kingsley550@yahoo.com>
2022-09-20 11:13:05 -0500

    Create group controller

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const { PORT, MONGODB_URI } = process.env;
 const testController = require("./controllers/test_controller");
 const authController = require("./controllers/auth_controller");
 const commentController = require("./controllers/comments_controller");
+const groupController = require("./controllers/group_controller");
+
 // const cors = require("cors");
 // const morgan = require("morgan");
 
@@ -20,17 +22,13 @@ app.use(express.json());
 app.use("/test", testController);
 app.use("/auth", authController);
 app.use("/comment", commentController);
+app.use("/group", groupController);
 
 // Routes
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-
-// app.get("/:ext", (req, res) => {
-//   //res.send("Hello world");
-//   res.redirect("/test/:ext")
-// });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
