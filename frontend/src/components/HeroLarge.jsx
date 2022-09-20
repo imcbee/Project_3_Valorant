7756f3ccc18994cb522377d8a69655afb7c3de59
estrellalvarez <alvare89@uwm.edu>
2022-09-19 22:35:49 -0500

    Home Styling

--- frontend/src/components/HeroLarge.jsx
+++ frontend/src/components/HeroLarge.jsx
@@ -7,17 +7,16 @@ export default function HeroLarge() {
                 <p>Check Detailed Valorant Stats and Leaderboards</p>
                 <img src="/images/Neon_Hero.webp"></img>
             </div> */}
-      
-      <div class="v3_7"></div>
-      <div class="v3_8"></div>
-      <div class="v3_9"></div>
-      <input class="v3_15" placeholder="Player#NA1" />
+      <div class="Hero">
+        <button class="v7_38">Sign In</button>
+        <div class="v3_7"></div>
+        <div class="v3_8"></div>
+        <div class="v3_9"></div>
+        <input class="v3_15" placeholder="Player#NA1" />
+        </div>
 
       <div class="v7_36"></div>
-
-      <button class="v7_38">Sign In</button>
-      <div class="v7_43"></div>
-      <span class="v7_42">Leaderboards</span>
+      <div class="v7_43"><span>Leaderboards</span></div>
       <span class="v7_46">VALORANT TRACKER</span>
       <span class="v7_48">Check Detailed Valorant Stats and Leaderboards</span>
     </>
