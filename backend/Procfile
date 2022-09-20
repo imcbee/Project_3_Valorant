131782bd3d8e15288210e05df80223b0e22d3337
badMeme <noveladdress@gmail.com>
2022-09-20 13:46:59 -0500

    lol more heroku

--- backend/Procfile
+++ backend/Procfile
@@ -1,1 +1,1 @@
-web: nodeserver.js
+web: node server.js
--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ const authController = require("./controllers/auth_controller");
 const commentController = require("./controllers/comments_controller");
 const groupController = require("./controllers/group_controller");
 
-// const cors = require("cors");
-// const morgan = require("morgan");
+const cors = require("cors");
+const morgan = require("morgan");
 
 //Database Connection
 mongoose.connect(MONGODB_URI);
 
 // Middleware
 app.use(express.json());
-// app.use(cors());
-// app.use(morgan("dev"));
+app.use(cors());
+app.use(morgan("dev"));
 app.use("/test", testController);
 app.use("/auth", authController);
 app.use("/comment", commentController);
