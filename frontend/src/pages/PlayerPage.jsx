2ee7f64fa15ee795679d853ac38a48f28def58a4
Ian <imcbee@terpmail.umd.edu>
2022-09-21 16:02:27 -0400

    working on playerpage

--- frontend/src/pages/PlayerPage.jsx
+++ frontend/src/pages/PlayerPage.jsx
@@ -3,19 +3,37 @@ import Footer from "../components/Footer";
 import HeroSmall from "../components/HeroLarge";
 import Comments from "../components/Comments";
 import OptionsNav from "../components/OptionsNav";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const URL = "https://val-halla.herokuapp.com/";
 
 export default function PlayerPage() {
   // fetch API data
   // do we need to have props sent from Main?
   // show page - map out player  stats
+  const [playerInfo, setPlayerInfo] = useState([]);
 
+  const getPlayer = async () => {
+    try {
+      const response = await fetch(URL);
+      console.log(response);
+      const data = await response.json();
+      setPlayerInfo(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getPlayer();
+  }, []);
+  console.log(playerInfo);
   return (
     <>
       <Header />
-      <div class="profile-banner"></div>
-      <div class="profile-card"></div>
-      <div class="match-cards"></div>
+      <div className="profile-banner"></div>
+      <div className="profile-card"></div>
+      <div className="match-cards"></div>
       <Footer />
     </>
   );
