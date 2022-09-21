521fd49d81ce10c1115753111dfbc4188937a3ba
Ian <imcbee@terpmail.umd.edu>
2022-09-21 12:46:52 -0400

    displayed data on homepage

--- frontend/src/pages/PlayerPage.jsx
+++ frontend/src/pages/PlayerPage.jsx
@@ -3,11 +3,19 @@ import Footer from "../components/Footer";
 import HeroSmall from "../components/HeroLarge";
 import Comments from "../components/Comments";
 import OptionsNav from "../components/OptionsNav";
+import { useState } from "react";
 
 export default function PlayerPage() {
   // fetch API data
   // do we need to have props sent from Main?
   // show page - map out player  stats
+
+  // Leader player state
+  const [leaderPlayers, setLeaderPlayers] = useState();
+
+  const handleChange = (e) => {
+    setLeaderPlayers({ ...leaderPlayers, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
